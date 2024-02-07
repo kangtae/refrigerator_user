76df47872b4ce93c290e09a9c6491c4907659259
kangtaewan <bras1717@naver.com>
2024-02-07 22:44:57 +0900

    아이디, 이름, 이메일, 비밀번호 마크업

--- src/app/signup/_components/LoginForm.tsx
+++ src/app/signup/_components/LoginForm.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm,  } from "react-hook-form";
 
 interface FormValue {
 	email: string;
 	password: string;
+	userId: string;
+	name: string;
 }
 
 
@@ -21,6 +23,42 @@ const LoginForm = () => {
 	} = useForm<FormValue>({ mode: "onChange" });
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className="form-control__items">
+				<label htmlFor="password">아이디</label>
+				<input
+					id="userId"
+					type="text"
+					placeholder="아이디를 입력해주세요."
+					{...register("user_id", {
+						required: "아이디는 필수 입력입니다.",
+						minLength: {
+							value: 4,
+							message: "4자리 이상 아이디를 입력하세요",
+						},
+					})}
+				/>
+				{errors.userId && (
+					<small role="alert">{errors.userId.message}</small>
+				)}
+			</div>
+			<div className="form-control__items">
+				<label htmlFor="password">이름</label>
+				<input
+					id="name"
+					type="text"
+					placeholder="ex) 홍길동"
+					{...register("user_id", {
+						required: "이름은 필수 입력입니다.",
+						minLength: {
+							value: 3,
+							message: "3자리 이상 이름 입력하세요.",
+						},
+					})}
+				/>
+				{errors.name && (
+					<small role="alert">{errors.name.message}</small>
+				)}
+			</div>
 			<div>
 				<label htmlFor="email">이메일</label>
 				<input
@@ -37,6 +75,24 @@ const LoginForm = () => {
 					})}
 				/>
 				{errors.email && <small role="alert">{errors.email.message}</small>}
+			</div>
+			<div className="form-control__items">
+				<label htmlFor="password">비밀번호</label>
+				<input
+					id="password"
+					type="password"
+					placeholder="*******"
+					{...register("password", {
+						required: "비밀번호는 필수 입력입니다.",
+						minLength: {
+							value: 7,
+							message: "7자리 이상 비밀번호를 입력하세요.",
+						},
+					})}
+				/>
+				{errors.password && (
+					<small role="alert">{errors.password.message}</small>
+				)}
 			</div>
 		</form>
 	)
