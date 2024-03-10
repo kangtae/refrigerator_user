430c8c0b9e2aa9caeeec3a62cec90ff7f2879996
kangtaewan <bras1717@naver.com>
2024-03-10 22:15:10 +0900

    food font-icon 등록

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,7 +5,7 @@ import SignInForm from "@/app/signIn/_components/SignInForm";
 const SignIn = () => {
 	return (
 		<>
-			<h1>로그인 페이지</h1>
+			<h1>로그인 페이지 <i className={"food-icons8-apple-fruit-100"}></i></h1>
 			<br/><br/>
 			<SignInForm/>
 		</>
