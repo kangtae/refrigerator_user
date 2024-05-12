726fa5ccc5cc2d0d1be0613bcbc777e2a18616cd
kangtaewan <bras1717@naver.com>
2024-05-12 20:55:29 +0900

    로그인 페이지 스타일 수정

--- src/app/signin/_components/SignInForm.tsx
+++ src/app/signin/_components/SignInForm.tsx
@@ -40,45 +40,65 @@ const SignInForm = () => {
 
 	}
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<div className="form-control__items">
-				<label htmlFor="userId">아이디</label>
-				<input
-					id="userId"
-					type="text"
-					placeholder="아이디를 입력해주세요."
-					{...register("userId", {
-						required: "아이디는 필수 입력입니다.",
-						minLength: {
-							value: 4,
-							message: "4자리 이상 아이디를 입력하세요",
-						},
-					})}
-				/>
-				{errors.userId && (
-					<small role="alert">{errors.userId.message}</small>
-				)}
+		<>
+			<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+				<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+					<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">로그인</h2>
+				</div>
+				<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+					<form class="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<label htmlFor="userId" className="block text-sm font-medium leading-6 text-gray-900">아이디</label>
+							<div class="mt-2">
+							<input
+								id="userId"
+								type="text"
+								class="block w-full pl-[10px] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								placeholder="아이디를 입력해주세요."
+								{...register("userId", {
+									required: "아이디는 필수 입력입니다.",
+									minLength: {
+										value: 4,
+										message: "4자리 이상 아이디를 입력하세요",
+									},
+								})}
+							/>
+							{errors.userId && (
+								<small role="alert" class="text-red-500">{errors.userId.message}</small>
+							)}
+							</div>
+						</div>
+						<div>
+							<div class="flex items-center justify-between">
+								<label for="password" class="block text-sm font-medium leading-6 text-gray-900">비밀번호</label>
+							</div>
+							<div class="mt-2">
+								<input
+									id="password"
+									type="password"
+									placeholder="*******"
+									class="block w-full pl-[10px] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									{...register("password", {
+										required: "비밀번호는 필수 입력입니다.",
+										minLength: {
+											value: 7,
+											message: "7자리 이상 비밀번호를 입력하세요.",
+										},
+									})}
+								/>
+							</div>
+							{errors.password && (
+								<small role="alert" class="text-red-500">{errors.password.message}</small>
+							)}
+						</div>
+
+						<div>
+							<button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+						</div>
+					</form>
+				</div>
 			</div>
-			<div className="form-control__items">
-				<label htmlFor="password">비밀번호</label>
-				<input
-					id="password"
-					type="password"
-					placeholder="*******"
-					{...register("password", {
-						required: "비밀번호는 필수 입력입니다.",
-						minLength: {
-							value: 7,
-							message: "7자리 이상 비밀번호를 입력하세요.",
-						},
-					})}
-				/>
-				{errors.password && (
-					<small role="alert">{errors.password.message}</small>
-				)}
-			</div>
-			<button type="submit">로그인</button>
-		</form>
+		</>
 	)
 };
 
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,8 +5,6 @@ import SignInForm from "@/app/signIn/_components/SignInForm";
 const SignIn = () => {
 	return (
 		<>
-			<h1>로그인 페이지 <i className={"food-icons8-apple-fruit-100"}></i></h1>
-			<br/><br/>
 			<SignInForm/>
 		</>
 	)
