c3c8a9c7c5d98f382cd6293d0d68b4bfd3b59d98
kangtaewan <bras1717@naver.com>
2024-05-14 20:32:21 +0900

    커밋 push 테스트

--- src/app/(afterLogin)/product/_components/ModalImgUrl.tsx
+++ src/app/(afterLogin)/product/_components/ModalImgUrl.tsx
@@ -20,6 +20,7 @@ const ModalImgUrl = ({onClose, onSubmit,defaultValue}) => {
 							<div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
 								<h3 className="text-xl font-semibold text-gray-900 dark:text-white">
 									제품 이미지
+
 								</h3>
 							</div>
 							<div className="p-4 md:p-5 space-y-4">
